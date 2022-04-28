977ea7104edc8c44c1fcd20d1d88831d35cfd4cd
nitishranjan27 <nk47595@gmail.com>
2022-04-28 22:39:15 +0530

    Nitish Created and Modified Api for Registration in FoundooNotes_Angular

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/userService/user.service';
 
 
 @Component({
@@ -11,28 +12,43 @@ export class RegistrationComponent implements OnInit {
   registerForm!: FormGroup;
     submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private user : UserService) { }
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      username: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required]
+      // service : "advance"
   });
 
 }
 
 onSubmit() {
     this.submitted = true;
+    console.log("Submited Successfully", this.registerForm.value);
 
     // stop here if form is invalid
-    if (this.registerForm.invalid) {
-        return;
+    if (this.registerForm.valid) {
+        // return;
+        console.log("Data inserted Successfully");
+      let reqData={
+        FirstName:this.registerForm.value.firstName,
+        LastName:this.registerForm.value.lastName,
+        Email:this.registerForm.value.email,
+        Password:this.registerForm.value.password
+        // service : this.registerForm.value.service
+      }
+      this.user.Registration(reqData).subscribe((response:any)=> {
+        console.log(response);
+      });
     }
+    
+    
 
     // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+    // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
 }
 }
